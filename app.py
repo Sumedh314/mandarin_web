f8308ce3b600e447567c7380357145930ba39953
Sumedh314 <sumedh3141@gmail.com>
2026-01-08 17:18:29 -0600

    Started experimenting with links

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/print_this')
+def print_this():
+    return render_template('index.html', text='hello')
+
 if __name__ == '__main__':
     app.run(debug=True)
