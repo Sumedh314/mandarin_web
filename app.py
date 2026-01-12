b35dc65eb504f84685504f439655efc618276077
Sumedh314 <sumedh3141@gmail.com>
2026-01-11 23:34:53 -0600

    Allows user to embed YouTube video by pasting link

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from deep_translator import GoogleTranslator
+
+translator = GoogleTranslator()
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/print_this')
-def print_this():
-    return render_template('index.html', text='hello')
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
