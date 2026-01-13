8c99877c16040c07f5224d1efdfeb48ad7377754
Sumedh314 <sumedh3141@gmail.com>
2026-01-12 18:12:20 -0600

    User can translate words or phrases by typing them

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 from deep_translator import GoogleTranslator
 
 translator = GoogleTranslator()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    phrase = request.data.decode('utf-8')
+    translation = (translator.translate(phrase))
+    result = {'translation': translation}
+    return(jsonify(result))
 
 if __name__ == '__main__':
     app.run(debug=True)
